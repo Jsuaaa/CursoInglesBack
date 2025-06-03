c4a6be28f613abb788e0316e062f3a0c06ac2b7e
JuanSBr <juanpablosuarezbrango@hotmail.com>
2025-06-02 19:40:57 -0500

    - Generación del proyecto.

---
+++ src/main/java/com/cursoinglesback/cursoinglesbackend/CursoinglesbackendApplication.java
@@ -0,0 +1,13 @@
+package com.cursoinglesback.cursoinglesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoinglesbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoinglesbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoinglesback/cursoinglesbackend/CursoinglesbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoinglesback.cursoinglesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoinglesbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
